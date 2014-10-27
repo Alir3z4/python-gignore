9b5cd4b1330267ab0ef7add049bd8585506f79e6
Alireza Savand <alireza.savand@gmail.com>
2014-10-28 03:32:04 +0400

    Add name attribute with setter/getter

--- gignore/__init__.py
+++ gignore/__init__.py
@@ -10,9 +10,22 @@ def get_version():
 
 class Gignore(object):
     BASE_URL = 'https://raw.githubusercontent.com/github/gitignore/master/'
+    name = None
 
     def get_base_url(self):
         """
         :rtype: str
         """
         return self.BASE_URL
+
+    def set_name(self, name):
+        """
+        :type name: str
+        """
+        self.name = name
+
+    def get_name(self):
+        """
+        :rtype: str
+        """
+        return self.name
